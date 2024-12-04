66b12dd4554ce831982f493fe8f2744c1b5934f6
Junyoung190198 <127806321+Junyoung190198@users.noreply.github.com>
2024-12-04 17:01:19 +0100

    Created all necessary env variables and login for the sql database. Also created express server and installed cors, dotenv, nodemon, squelize and tedious. Now need to begin the sequelize implementation

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const cors = require('cors');
 
+
+
+const app = express(); // set up express server
+
+// allow request to all origins (Need to update if going to productions: security mesures)
+app.use(cors()); 
+// set JSON parser so able to use req.body
+app.use(express.json());
+
+
+
+
+module.exports = app;
